e44ba1e456ce49c0b35b19b45aead41a6ffe9045
suryasmom <suryasmom11@gmail.com>
2022-03-18 20:03:36 -0400

    changed a bit in fishy, area and homepage

---
+++ area.py
@@ -0,0 +1,11 @@
+print('Welcome to my rectangle area calculation program!')
+#We need to get the width from the user
+width = input("What's your rectangle's width in inches?")
+#Turning the value into a number
+width = int(width)
+height = input("What's your rectangle's height inches?")
+height = int(height)
+area = width * height
+print("Your rectangle's area is", area, "inches squared.")
+
+
