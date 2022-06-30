18224af46688e0234295f30a7ae3bb028263145f
Green <87390224+coder175@users.noreply.github.com>
2022-06-30 18:07:04 -0400

    updates

---
+++ sum.js
@@ -0,0 +1,8 @@
+function sum(number1, number2) {
+ return number1+number2;
+}
+
+let result = sum(3.14, 6.7899);
+console.log(result);
+console.log(sum(4.56, 10));
+console.log(sum(4.5,6.7)+sum(5,6));
