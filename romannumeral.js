8d477b5108341623e1eafc872e8b654ab21e6cd4
Green <87390224+coder175@users.noreply.github.com>
2022-07-25 18:30:11 -0400

    maintenance issues

--- romannumeral.js
+++ romannumeral.js
@@ -1,9 +1,9 @@
 function romanNumeralToInt(str) {
-    if(str == null) return console.log(" you did send me the input - null");
+    if(!str) return console.log(" you did send me the input - null");
     let numerals = new Map([['I', 1], ['V', 5], ['X', 10], ['L', 50], ['C', 100], ['D', 500], ['M', 1000]]);
-    let result = numerals.get(str[(str.length)-1]); // finds the rightmost character in the string.
-    for (let i = str - 2; i >= 0; i--) {
-        if (numerals.get(str[i]) < numerals.get(str[i - 1])) {
+    let result = numerals.get(str[str.length - 1]); // finds the rightmost character in the string.
+    for (let i = str.length - 2; i >= 0; i--) {
+        if (numerals.get(str[i]) < numerals.get(str[i + 1])) {
             result -= numerals.get(str[i]);
         }
         else {
@@ -14,6 +14,8 @@ function romanNumeralToInt(str) {
     console.log(str, result);
     return result;
 }
+romanNumeralToInt('');
+romanNumeralToInt();
 romanNumeralToInt('XXIV');
 romanNumeralToInt('XXVI');
 romanNumeralToInt('MXXXVIII');
