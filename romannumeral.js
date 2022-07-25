3b8bf0d6bf6f473d6d8b5b525949828661372c36
Green <87390224+coder175@users.noreply.github.com>
2022-07-25 18:21:55 -0400

    deleted html made js made program better

---
+++ romannumeral.js
@@ -0,0 +1,20 @@
+function romanNumeralToInt(str) {
+    if(str == null) return console.log(" you did send me the input - null");
+    let numerals = new Map([['I', 1], ['V', 5], ['X', 10], ['L', 50], ['C', 100], ['D', 500], ['M', 1000]]);
+    let result = numerals.get(str[(str.length)-1]); // finds the rightmost character in the string.
+    for (let i = str - 2; i >= 0; i--) {
+        if (numerals.get(str[i]) < numerals.get(str[i - 1])) {
+            result -= numerals.get(str[i]);
+        }
+        else {
+            result += numerals.get(str[i]);
+        }
+
+    }
+    console.log(str, result);
+    return result;
+}
+romanNumeralToInt('XXIV');
+romanNumeralToInt('XXVI');
+romanNumeralToInt('MXXXVIII');
+romanNumeralToInt('MDCLXVI');
